961d2ff7159beaac43e69dd518b3e0f69000302b
Hanks <zhanghan.me@gmail.com>
2017-02-26 00:15:34 +0800

    - [jsfm] build vue and vanilla only

--- html5/frameworks/index.js
+++ html5/frameworks/index.js
@@ -1,11 +1,7 @@
 import * as Vanilla from './vanilla/index'
 import * as Vue from 'weex-vue-framework'
-import * as Weex from './legacy/index'
-import Rax from 'weex-rax-framework'
 
 export default {
   Vanilla,
-  Vue,
-  Rax,
-  Weex
+  Vue
 }
